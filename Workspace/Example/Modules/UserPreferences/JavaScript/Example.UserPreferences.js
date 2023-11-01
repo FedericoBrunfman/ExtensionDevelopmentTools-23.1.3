cec5313bab501343b4a4dec971a2d733e4ed6612
Federico Brunfman <federicobrunfman@gmail.com>
2023-11-01 17:58:11 -0300

    first extension

---
+++ Workspace/Example/Modules/UserPreferences/JavaScript/Example.UserPreferences.js
@@ -0,0 +1,13 @@
+define('Example.UserPreferences', [
+
+], function (
+
+) {
+    'use strict';
+
+    return {
+        mountToApp: function (container) {
+            console.log('Hello World! UserPreferences calling!');
+        }
+    }
+});
